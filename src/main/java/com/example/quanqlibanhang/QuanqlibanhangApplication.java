cbafb6f63b96934246c78016dc22c47bd737e250
tuanptit2002 <tuantienti952002@gmail.com>
2023-03-08 23:12:18 +0700

    add new comment

--- src/main/java/com/example/quanqlibanhang/QuanqlibanhangApplication.java
+++ src/main/java/com/example/quanqlibanhang/QuanqlibanhangApplication.java
@@ -7,7 +7,7 @@ import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class QuanqlibanhangApplication {
-
+    // this is main code
     public static void main(String[] args) {
         SpringApplication.run(QuanqlibanhangApplication.class, args);
         System.out.println("Phai Thanh Cong");
